621d19e8dd331b85f731b0785e8f708999590a1c
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-12-19 17:32:55 +0100

    made changes

--- components/Create/ProductForm.vue
+++ components/Create/ProductForm.vue
@@ -1,5 +1,7 @@
 <template>
-
+  <div class="form">
+    its working
+  </div>
 </template>
 
 <script>
--- pages/create/product.vue
+++ pages/create/product.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="product">
-    
+    <Form />
   </div>
 </template>
 
 <script>
+import Form from "~/components/Create/ProductForm.vue"
 export default {
-
+  components: {
+    Form,
+  }
 }
 </script>
 
