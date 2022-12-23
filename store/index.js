dad796dd7c026bf7e60e83ba9b715ac87ad8d411
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-12-23 21:56:59 +0100

    made changes

--- store/index.js
+++ store/index.js
@@ -3,13 +3,31 @@ export const state = () => ({
 })
 
 export const mutations = {
-
+  ON_AUTH_STATE_CHANGED_MUTATION(state, { authUser, claims }) {
+    if (!authUser) {
+      // claims = null
+      // perform logout operations
+      console.log(authUser, claims)
+    } else {
+      // Do something with the authUser and the claims object...
+      const { uid, email, emailVerified } = authUser
+      state.user = { uid, email, emailVerified }
+      console.log(authUser, claims)
+    }
+  }
 }
 
 export const actions = {
-
+  onAuthStateChangedAction() {
+    if (!authUser) {
+      // claims = null
+      // Perform logout operations
+    } else {
+      // Do something with the authUser and the claims object...
+    }
+  }
 }
 
 export const modules = {
-  
+
 }
