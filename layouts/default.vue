d61f5ee574aee6711dd4efc5812b27b1f976098c
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-12-23 23:30:55 +0100

    made changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,18 @@
 </template>
 
 <script>
+import {mapGetters} from 'vuex'
 export default {
-
+  computed: {
+    ...mapGetters({
+      isLoggedIn: 'isLoggedIn',
+    }),
+  },
+  mounted() {
+    if(!this.isLoggedIn) {
+      this.$router.push('/login')
+      return
+    }
+  }
 }
 </script>
