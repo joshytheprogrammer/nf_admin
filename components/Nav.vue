5fd79e4491ad1703391791eb57c991ac5aaa2c24
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-12-24 00:07:38 +0100

    made changes

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,26 +1,26 @@
 <template>
-  <b-navbar>
+  <b-navbar fixed-top>
     <template #brand>
       <b-navbar-item tag="router-link" :to="{ path: '/' }">
         <span class="title has-text-primary">Neas Fashion</span>
       </b-navbar-item>
-  </template>
-  <template #end>
-    <b-navbar-item href="/">
-      Home
-    </b-navbar-item>
-    <b-navbar-dropdown label="Create">
-      <b-navbar-item href="/create/product">
-        Product
+    </template>
+    <template #end>
+      <b-navbar-item href="/">
+        Home
       </b-navbar-item>
-      <b-navbar-item href="/create/category">
-        Category
+      <b-navbar-dropdown label="Create">
+        <b-navbar-item href="/create/product">
+          Product
+        </b-navbar-item>
+        <b-navbar-item href="/create/category">
+          Category
+        </b-navbar-item>
+      </b-navbar-dropdown>
+      <b-navbar-item href="/#" @click.prevent="logout">
+        Logout
       </b-navbar-item>
-    </b-navbar-dropdown>
-    <b-navbar-item href="/#" @click.prevent="logout">
-      Logout
-    </b-navbar-item>
-  </template>
+    </template>
   </b-navbar>
 </template>
 
