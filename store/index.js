f64c986a2ea70b63107d489e214bc6555a4b8b8d
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-12-23 23:16:52 +0100

    made changes

--- store/index.js
+++ store/index.js
@@ -1,28 +1,44 @@
 export const state = () => ({
-  user: {
-
-  }
+  user: null
 })
 
 export const mutations = {
+  RESET_STORE: (state) => {
+    Object.assign(state, {user: null})
+  },
 
-}
-
-export const actions = {
-  onAuthStateChangedAction(ctx, { authUser, claims }) {
-    if (!authUser) {
-      // claims = null
-      // Perform logout operations
-      console.log(authUser, claims)
-    } else {
-      // Do something with the authUser and the claims object...
-      const { uid, email, emailVerified } = authUser
-      // state.user = { uid, email, emailVerified }
-      console.log(authUser, claims)
+  SET_AUTH_USER: (state, { authUser }) => {
+    state.user = {
+      uid: authUser.uid,
+      email: authUser.email
     }
   }
 }
 
-export const modules = {
+export const actions = {
+  async onAuthStateChangedAction({ commit }, { authUser }) {
+    if (!authUser) {
+      commit('RESET_STORE')
+      return
+    }
+    if (authUser && authUser.getIdToken) {
+      try {
+        const idToken = await authUser.getIdToken(true)
+        console.info('idToken', idToken)
+      } catch (e) {
+        console.error(e)
+      }
+    }
+    commit('SET_AUTH_USER', { authUser })
+  },
+}
 
+export const getters = {
+  isLoggedIn: (state) => {
+    try {
+      return state.user.uid !== null
+    } catch {
+      return false
+    }
+  }
 }
