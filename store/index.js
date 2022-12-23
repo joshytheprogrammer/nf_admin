d64bc17eae812a440aa204488d6e592ec2c5dfe4
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-12-23 23:00:17 +0100

    made changes

--- store/index.js
+++ store/index.js
@@ -1,22 +1,11 @@
 export const state = () => ({
   user: {
-    
+
   }
 })
 
 export const mutations = {
-  ON_AUTH_STATE_CHANGED_MUTATION(state, { authUser, claims }) {
-    if (!authUser) {
-      // claims = null
-      // perform logout operations
-      console.log(authUser, claims)
-    } else {
-      // Do something with the authUser and the claims object...
-      const { uid, email, emailVerified } = authUser
-      state.user = { uid, email, emailVerified }
-      console.log(authUser, claims)
-    }
-  }
+
 }
 
 export const actions = {
@@ -24,8 +13,12 @@ export const actions = {
     if (!authUser) {
       // claims = null
       // Perform logout operations
+      console.log(authUser, claims)
     } else {
       // Do something with the authUser and the claims object...
+      const { uid, email, emailVerified } = authUser
+      // state.user = { uid, email, emailVerified }
+      console.log(authUser, claims)
     }
   }
 }
