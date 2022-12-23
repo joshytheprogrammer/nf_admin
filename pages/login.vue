c921b728f00a5441bd5c6abf73088305948d7575
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-12-23 22:04:41 +0100

    made changes

---
+++ pages/login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="login">
+    <b-field label="Email" type="is-danger" message="This email is invalid">
+      <b-input type="email" maxlength="30" v-model="user.email"></b-input>
+    </b-field>
+
+    <b-field label="Password">
+      <b-input type="password" v-model="user.password" password-reveal></b-input>
+    </b-field>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    async login() {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
